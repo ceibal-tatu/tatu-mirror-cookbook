837741827013cd129caca73454e6e773c1a3da5b
Santiago Aguiar <santiago.aguiar@gmail.com>
2014-11-09 17:40:49 +0000

    Basic mirror recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,42 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+
+cookbook_file '/etc/ssh/sshd_config' do
+    source 'sshd_config'
+    owner 'root'
+    group 'root'
+    mode '644'
+    notifies :reload, 'service[ssh]'
+end
+
+service 'ssh' do 
+    action [:enable, :start]
+end
+
+cookbook_file '/root/.ssh/authorized_keys' do
+    source 'ssh_authorized_keys'
+    owner 'root'
+    group 'root'
+    mode '600'
+end
+
+cookbook_file '/etc/apt/sources.list.d/tatu.list' do
+    source 'tatu.list'
+    owner 'root'
+    group 'root'
+    mode '644'
+end
+
+execute 'wget tatu.gpg.key' do
+    command 'wget http://tatumaster.ddns.net/tatu.gpg.key -O - |apt-key add -'
+end
+
+package 'tatu-repo-devel'
+
+cookbook_file '/etc/cron.d/tatu-rsync-mirror' do
+    source 'tatu-rsync-mirror'
+    owner 'root'
+    group 'root'
+    mode '644'
+end
